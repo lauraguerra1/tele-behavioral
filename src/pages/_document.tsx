22f6ad1f2d9c5d4896fd25bfe498dc292a73b35e
Laura Guerra <l.garciaguerra1@gmail.com>
2023-11-05 16:30:24 -0500

    Make form bg lighter opacity and add cairn bg to body

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300&display=swap" rel="stylesheet"/>
       </Head>
-      <body>
+      <body className='bg-cover'>
         <Main />
         <NextScript />
       </body>
