5ac6662c928411279111cba73c9a153dd24563d1
Laura Guerra <l.garciaguerra1@gmail.com>
2023-11-06 10:52:57 -0500

    Change to playfair display

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Form from '@/components/Form'
-import gradient from '../images/gradient.png'
 import ContactCard from '@/components/ContactCard'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -12,8 +10,9 @@ export default function Home() {
       <section id='nav' className='w-screen bg-white sticky top-0 z-20 border-b-4 border-great-gray p-5'>
         <h1 className='text-5xl text-center text-blackish-gray'>TELE BEHAVIORAL HEALTH</h1>
       </section>
-      <section id='intro' className='w-87vw self-center bg-cover h-80 mb-5'>
-        <p>intro will go here</p>
+      <section id='intro' className='w-87vw self-center bg-cover h-650px mb-5 flex flex-col items-center justify-around py-20'>
+        <p className='text-7xl text-center'>PUT YOUR <br/>MIND + BODY + SPIRIT<br/> IN GOOD HANDS</p>
+        <p className='text-center italic playfair'>CREATIVE EMPOWERMENT THROUGH <br/>LOVE, SUPPORT, AND EDUCATION</p>
       </section>
       <section id='philosophy' className='w-screen self-center bg-white h-80 mb-10'>
         <p>philosophy will go here</p>
