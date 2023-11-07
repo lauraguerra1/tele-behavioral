1e55d6d0b96ceb5f433c9cb2ec293eefb77e436e
Laura Guerra <l.garciaguerra1@gmail.com>
2023-11-07 14:45:15 -0500

    Make form controlled

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,9 @@
 import { FormEvent, useEffect, useState } from "react"
-type Submission = {
+type Indexable = {
+  [index:string]: string
+}
+
+type Submission = Indexable & {
   name: string, 
   email: string, 
   subject: string,
@@ -17,15 +21,15 @@ const Form = () => {
   })
 
   useEffect(() => {
-    console.log('form data', formData)
+
     return resetFeedback
-  },[formData])
+  },[])
 
   const submitForm = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     try {
       //await api call work here 
-      // console.log({ ...formData, subject: `Tele Behavioral${formData.subject.length ? ': ' + formData.subject : ' Contact Request'}` })
+      console.log({ ...formData, subject: `Tele Behavioral${formData.subject.length ? ': ' + formData.subject : ' Contact Request'}` })
       setSuccess(true)
       setFormData({
         name: '', 
@@ -55,14 +59,14 @@ const Form = () => {
       <div key={field} className={`${field === 'message' || field === 'subject' ? 'col-span-2 flex-col' : ''} flex border-b-1 border-white`}>
         <label className='pr-2' htmlFor={field}>{`${field.charAt(0).toUpperCase()}${field.slice(1)}${field !== 'subject' ? '*' : ''}`}</label>
         {field !== 'message'
-          ? <input className='focus:outline-none bg-transparent' type={field === 'email' ? 'email' : 'text'} id={field} name={field} onChange={(e) => changeInput(e.target.name, e.target.value)} required={field !== 'subject'} />
-          : <textarea  className='focus:outline-none bg-transparent h-32 resize-none' id={field} name={field} onChange={(e) => changeInput(e.target.name, e.target.value)} required/>}
+          ? <input className='focus:outline-none bg-transparent' type={field === 'email' ? 'email' : 'text'} id={field} name={field} onChange={(e) => changeInput(e.target.name, e.target.value)} onFocus={resetFeedback} value={formData[field]} required={field !== 'subject'} />
+          : <textarea className='focus:outline-none bg-transparent h-32 resize-none' id={field} name={field} onChange={(e) => changeInput(e.target.name, e.target.value)} onFocus={resetFeedback} value={formData[field]} required/>}
       </div>
     )
   })
 
   return (
-    <form onFocus={resetFeedback} onSubmit={submitForm} className='grid grid-cols-2 gap-4 text-white mt-10'>
+    <form onSubmit={submitForm} className='grid grid-cols-2 gap-4 text-white mt-10'>
       {formInputs}
       <button className="col-span-2">Send</button>
       <p className={`text-center col-span-2 h-10 ${success? 'text-gray-700' : error ? 'text-red-700' : ''}`}>{success ? 'Success! Message sent.' : error ? 'Whoops! Something went wrong. Please try again.' : ''}</p>
