62c31552b68abe62d06b1d3223d216714a243339
Laura Guerra <l.garciaguerra1@gmail.com>
2023-11-07 14:37:16 -0500

    Add success and error resets

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { FormEvent, useEffect, useState } from "react"
 type Submission = {
   name: string, 
   email: string, 
@@ -16,7 +16,34 @@ const Form = () => {
     message: ''
   })
 
-  const inputChange = (field:string, input: string) => {
+  useEffect(() => {
+    console.log('form data', formData)
+    return resetFeedback
+  },[formData])
+
+  const submitForm = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    try {
+      //await api call work here 
+      // console.log({ ...formData, subject: `Tele Behavioral${formData.subject.length ? ': ' + formData.subject : ' Contact Request'}` })
+      setSuccess(true)
+      setFormData({
+        name: '', 
+        email: '', 
+        subject: '',
+        message: ''
+      })
+    } catch (error) {
+      setError(true)
+    }
+  }
+
+  const resetFeedback = () => {
+    setError(false)
+    setSuccess(false)
+  }
+
+  const changeInput = (field:string, input: string) => {
     setFormData(prev => ({
       ...prev, 
       [field]: input
@@ -28,14 +55,14 @@ const Form = () => {
       <div key={field} className={`${field === 'message' || field === 'subject' ? 'col-span-2 flex-col' : ''} flex border-b-1 border-white`}>
         <label className='pr-2' htmlFor={field}>{`${field.charAt(0).toUpperCase()}${field.slice(1)}${field !== 'subject' ? '*' : ''}`}</label>
         {field !== 'message'
-          ? <input className='focus:outline-none bg-transparent' type='text' id={field} name={field} onChange={(e) => inputChange(e.target.name, e.target.value)} required={field !== 'subject'} />
-          : <textarea  className='focus:outline-none bg-transparent h-32 resize-none' id={field} name={field} onChange={(e) => inputChange(e.target.name, e.target.value)} required/>}
+          ? <input className='focus:outline-none bg-transparent' type={field === 'email' ? 'email' : 'text'} id={field} name={field} onChange={(e) => changeInput(e.target.name, e.target.value)} required={field !== 'subject'} />
+          : <textarea  className='focus:outline-none bg-transparent h-32 resize-none' id={field} name={field} onChange={(e) => changeInput(e.target.name, e.target.value)} required/>}
       </div>
     )
   })
 
   return (
-    <form className='grid grid-cols-2 gap-4 text-white mt-10'>
+    <form onFocus={resetFeedback} onSubmit={submitForm} className='grid grid-cols-2 gap-4 text-white mt-10'>
       {formInputs}
       <button className="col-span-2">Send</button>
       <p className={`text-center col-span-2 h-10 ${success? 'text-gray-700' : error ? 'text-red-700' : ''}`}>{success ? 'Success! Message sent.' : error ? 'Whoops! Something went wrong. Please try again.' : ''}</p>
