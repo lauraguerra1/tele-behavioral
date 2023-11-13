7854245f1394d7c393ba58460fe4b9752cd5a06d
Laura Guerra <l.garciaguerra1@gmail.com>
2023-11-13 15:35:51 -0500

    Fix error handling in api/mail

--- src/pages/api/mail.js
+++ src/pages/api/mail.js
@@ -1,18 +1,8 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 const nodemailer = require('nodemailer');
-
-
 
 export default function mail(req, res) {
   const { name, email, subject, message } = req.body
-  console.log('req body', req.body)
-  const missingParams = ['name', 'email', 'subject', 'message'].filter(requiredParam => !req.body[requiredParam])
-  
-  if (missingParams.length) {
-    return res.status(422).json({ error: `Missing required param of ${missingParams.join(', ')}.` })
-  }
 
-  //rememeber to set up error handling for missing parts here 
   const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 465,
@@ -37,12 +27,9 @@ export default function mail(req, res) {
 
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
-      res.status(info.responseCode).json({error: info.response})
-      console.log('error', error)
+      res.status(500).json(error)
     } else {
       res.status(200).json({message: info.response})
-      console.log('info', info)
-      console.log('response', info.response)
     }
   })
 }
