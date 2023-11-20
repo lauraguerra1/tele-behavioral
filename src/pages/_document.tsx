ee64ea6d322763be7332138a7b413266463c746c
Laura Guerra <l.garciaguerra1@gmail.com>
2023-11-20 12:09:45 -0500

    Add meta tag with site description

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,7 @@ export default function Document() {
         <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@200;300&family=Playfair+Display&display=swap" rel="stylesheet" />
         <title>Roxanne Flaherty</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+        <meta name='description' content='Roxanne Flaherty is a Board Certified Family and Psychiatric Nurse Practitioner, offering a wide range of mental health services.' />
       </Head>
       <body className='bg-cover'>
         <Main />
