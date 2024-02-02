66b380e1bacd8eb1c8fc8bee07cdd5935deda67c
Laura Guerra <l.garciaguerra1@gmail.com>
2024-02-02 14:39:11 -0500

    Change states servciing to CA only

--- src/components/ContactCard.tsx
+++ src/components/ContactCard.tsx
@@ -1,26 +1,30 @@
-import Form from "./Form"
+import Form from './Form';
 
 const ContactCard = () => {
   return (
     <article className='text-white flex flex-col justify-around mb-15 mr-5'>
       <h2 className='text-6xl'>GET IN TOUCH</h2>
       <div id='contactInfo' className='flex flex-col'>
-        <section className="flex">
-        <div className='h-20 pr-5 border-r-1 border-white flex flex-col justify-center my-5'>
-          <p>States servicing:</p>
-          <p>CA & FL</p>
-        </div>
-        <div className='h-20 pl-5 flex flex-col justify-center break-words my-5'>
-          <p>Tel: 323-433-9935</p>
-          <p>Email: admin@roxanneflaherty.com</p>
-        </div>
+        <section className='flex'>
+          <div className='h-20 pr-5 border-r-1 border-white flex flex-col justify-center my-5'>
+            <p>States servicing:</p>
+            <p>CA*</p>
+          </div>
+          <div className='h-20 pl-5 flex flex-col justify-center break-words my-5'>
+            <p>Tel: 323-433-9935</p>
+            <p>Email: admin@roxanneflaherty.com</p>
+          </div>
         </section>
-        <p>Address: <br/> 11400 West Olympic Blvd Suite 200 <br /> Los Angeles, CA 90064</p>
-        <p className='mt-2 italic'>Services coming soon in NY & WA</p>
-    </div>
-    <a id='linkedInDesktop' className='bg-sandy text-gray-700 px-4 my-4 py-2 self-start' href='https://www.linkedin.com/in/roxanne-flaherty-881908298/' target='_blank'>Visit LinkedIn</a>
-  </article>
-  )
-}
+        <p>
+          Address: <br /> 11400 West Olympic Blvd Suite 200 <br /> Los Angeles, CA 90064
+        </p>
+        <p className='mt-2 italic'>*Services coming soon in FL, NY & WA</p>
+      </div>
+      <a id='linkedInDesktop' className='bg-sandy text-gray-700 px-4 my-4 py-2 self-start' href='https://www.linkedin.com/in/roxanne-flaherty-881908298/' target='_blank'>
+        Visit LinkedIn
+      </a>
+    </article>
+  );
+};
 
-export default ContactCard
+export default ContactCard;
